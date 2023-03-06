0b0d5396b4e83c414d0ee01622b62ee1f798f5a8
Anastasiia Ünal <velyka.nastya@icloud.com>
2023-03-06 16:48:12 +0100

    add time to summary

--- src/app/summary/summary.component.ts
+++ src/app/summary/summary.component.ts
@@ -3,13 +3,39 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
-  styleUrls: ['./summary.component.scss']
+  styleUrls: ['./summary.component.scss', './clock.scss']
 })
 export class SummaryComponent implements OnInit {
+  time;
+  hours;
+  greeting;
 
-  constructor() { }
+  constructor() {
+    setInterval(() => {
+      this.time = new Date();
+    }, 1000);
+
+    this.decide();
+  }
 
   ngOnInit(): void {
+  }
+
+
+  decide() {
+    this.hours = new Date().getHours();
+
+    if (this.hours < 10) {
+      this.greeting = "Good Morning";
+    } else if (this.hours < 16) {
+      this.greeting = "Good Afternoon";
+    } else if (this.hours < 19) {
+      this.greeting = "Good Evening";
+    } else if (this.hours < 24) {
+      this.greeting = "Good Night";
+    } else if (this.hours < 6) {
+      this.greeting = "Hello";
+    }
   }
 
 }
