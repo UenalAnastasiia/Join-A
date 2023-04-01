3838892c7f4b097da4123c1ead8a6fddbbd0809e
Anastasiia Ünal <velyka.nastya@icloud.com>
2023-04-01 18:19:44 +0200

    start drag drop in board

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,6 +24,7 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
@@ -50,6 +51,7 @@ import { provideFirestore,getFirestore } from '@angular/fire/firestore';
     FormsModule,
     MatDatepickerModule,
     MatNativeDateModule,
+    DragDropModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
     provideFirestore(() => getFirestore())
   ],
--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,7 +1,8 @@
+import { CdkDrag, CdkDragDrop, CdkDropList, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 import { collectionData, Firestore } from '@angular/fire/firestore';
 import { MatDialog } from '@angular/material/dialog';
-import { collection } from 'firebase/firestore';
+import { collection, query, where } from 'firebase/firestore';
 import { Observable } from 'rxjs';
 import { Task } from 'src/models/task.class';
 import { DialogAddTaskComponent } from '../dialog-add-task/dialog-add-task.component';
@@ -17,7 +18,30 @@ export class BoardComponent implements OnInit {
   allTasks: any = [];
   taskID: string;
 
+  todoTask: any;
+  inprogressTask: any = [];
+  awaitingfeedbackTask: any = [];
+  doneTask: any = [];
+
   statusList: any[] = ["To do", "In progress", "Awaiting Feedback", "Done"];
+  // statusList: any[] = [
+  //   {
+  //     name: "To do",
+  //     taskArray: "this.todoTask"
+  //   },
+  //   {
+  //     name: "In progress",
+  //     taskArray: "this.inprogressTask"
+  //   },
+  //   {
+  //     name: "Awaiting Feedback",
+  //     taskArray: "this.awaitingfeedbackTask"
+  //   },
+  //   {
+  //     name: "Done",
+  //     taskArray: "this.doneTask"
+  //   },
+  // ];
 
   constructor(public dialog: MatDialog, private firestore: Firestore) { }
 
@@ -28,6 +52,10 @@ export class BoardComponent implements OnInit {
     this.allTasks$.subscribe((loadData: any) => {
       this.allTasks = loadData;
     });
+
+    for (let index = 0; index < this.statusList.length; index++) {
+      this.filterTasks(this.statusList[index]);
+    }
   }
 
 
@@ -44,9 +72,45 @@ export class BoardComponent implements OnInit {
   }
 
 
+  filterTasks(name: string) {
+    const queryCollection = query(collection(this.firestore, "tasks"), where("status", "==", name));
+    this.allTasks$ = collectionData(queryCollection, { idField: "taskID" });
+    this.allTasks$.subscribe((data: any) => {
+      if (name === "To do") {
+        this.todoTask = data;
+      } else if (name === "In progress") {
+        this.inprogressTask = data;
+      } else if (name === "Awaiting Feedback") {
+        this.awaitingfeedbackTask = data;
+      } else if (name === "Done") {
+        this.doneTask = data;
+      }
+    });
+  }
+
+
+  drop(event: CdkDragDrop<string[]>, tt) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+        // console.log(event.previousIndex);
+        
+        this.updateTaskStatus(event.container.data[event.currentIndex]['id']);
+    }
+  }
+
+
+  updateTaskStatus(id: any) {
+    
+  }
+
+
   openDialogAddTask(status: string) {
     const dialog = this.dialog.open(DialogAddTaskComponent);
     dialog.componentInstance.taskStatus = status;
   }
-
 }
