a308426b9e66aed1eaa7aa27b925e889c67088ad
Anastasiia Ünal <velyka.nastya@icloud.com>
2023-04-05 11:46:21 +0200

    add routerlinkactive

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -10,43 +10,14 @@ import { DialogAddTaskComponent } from '../dialog-add-task/dialog-add-task.compo
 })
 export class NavbarComponent implements OnInit {
   btnTop = [
-    { name: 'summary', isClicked: true, icon: 'pending_actions' },
-    { name: 'board', isClicked: false, icon: 'dashboard' },
-    { name: 'contact', isClicked: false, icon: '3p' },
+    { name: 'summary', icon: 'pending_actions' },
+    { name: 'board', icon: 'dashboard' },
+    { name: 'contact', icon: '3p' },
   ];
 
   constructor(public router: Router, public dialog: MatDialog) { }
 
   ngOnInit(): void {
-    setInterval(() => {
-      this.checkURL();
-    });
-  }
-
-
-  checkURL() {
-    if (this.router.url.includes('summary')) {
-      this.btnTop[0].isClicked = true;
-      this.btnTop[1].isClicked = false;
-      this.btnTop[2].isClicked = false;
-    } else if (this.router.url.includes('board')) {
-      this.btnTop[1].isClicked = true;
-      this.btnTop[0].isClicked = false;
-      this.btnTop[2].isClicked = false;
-    } else if (this.router.url.includes('contact')) {
-      this.btnTop[2].isClicked = true;
-      this.btnTop[0].isClicked = false;
-      this.btnTop[1].isClicked = false;
-    }
-  }
-
-
-  setActive(button: any): void {
-    for (let but of this.btnTop) {
-      but.isClicked = false;
-    }
-
-    button.isClicked = true;
   }
 
 
